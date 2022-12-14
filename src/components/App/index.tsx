b3a34d1443dd1e8b3cb24e527987dcff2ee63971
anast-ananko <anast.ananko@gmail.com>
2022-12-15 00:13:16 +0200

    feat: add header in app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,29 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./App.css";
 
-import Home from '../Home';
-import Product from '../Product';
-import ProductGrid from '../ProductGrid';
-import About from '../About';
+import Header from "../Header/Header";
+import Home from "../Home";
+import Product from "../Product";
+import ProductGrid from "../ProductGrid";
+import About from "../About";
 
 const App = () => {
   return (
-    <> 
-      <ul className='list'>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/products'>Products</Link></li>
-        <li><Link to='/about'>About</Link></li>
-      </ul>
-      <hr />
+    <>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/products' element={<ProductGrid />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/products" element={<ProductGrid />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
