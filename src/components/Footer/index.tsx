3fe3587c9db92db302a506f4b064c4661057d040
anast-ananko <anast.ananko@gmail.com>
2022-12-16 01:11:38 +0200

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <nav className="footer__nav vl">
+            <ul className="footer__list">
+              <li className="footer__item">
+                <Link className="footer__link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="footer__item">
+                <Link className="footer__link" to="/products">
+                  Products
+                </Link>
+              </li>
+              <li className="footer__item">
+                <Link className="footer__link" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="footer__github vl">
+            <a
+              className="footer__github-link"
+              href="https://github.com/SiarheiKliazovich"
+            ></a>
+            <a
+              className="footer__github-link"
+              href="https://github.com/anast-ananko"
+            ></a>
+          </div>
+          <div className="footer__text">
+            <a className="footer__rs" href="https://rs.school/js/"></a>
+            <span className="footer__year">2023</span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
