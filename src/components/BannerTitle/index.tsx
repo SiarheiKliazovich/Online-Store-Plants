cb3373d3e5ea4cc631e50749119d6ac3e08ca65b
SiarheiKliazovich <alex849317@gmail.com>
2022-12-15 18:57:05 +0300

    feat: add BannerTitle and styles, add ProdGrid

---
+++ src/components/BannerTitle/index.tsx
@@ -0,0 +1,11 @@
+import "./BannerTitle.css";
+
+const BannerTitle = () => {
+  return (
+    <div className="banner">
+      <h1 className="banner__title">Products</h1>
+    </div>
+  );
+};
+
+export default BannerTitle;
--- src/components/ProductGrid/index.tsx
+++ src/components/ProductGrid/index.tsx
@@ -1,5 +1,27 @@
+import BannerTitle from "../BannerTitle";
+import products from "../../data/products";
+import "./ProductGrid.css";
+
 const ProductGrid = () => {
-  return <div>ProductGrid</div>;
+  const prodItems = products.map((elem) => elem);
+  console.log(prodItems);
+
+  return (
+    <>
+      <div className="main">
+        <BannerTitle />
+        <div className="main__wrapper">
+          <div className="container">
+            <div className="list_wrapper">
+              <ul className="prodlist">
+                <li className="prodlist_item">{}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductGrid;
