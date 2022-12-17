aa4bfe36eb74a3efcb1936c70d755a0355bd9f52
SiarheiKliazovich <alex849317@gmail.com>
2022-12-17 16:28:17 +0300

    feat: add path to probuct

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,7 @@
+import BannerTitle from "../BannerTitle";
+
+const Product = () => {
+  return <BannerTitle />;
+};
+
+export default Product;
---
+++ src/interface/interface.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  id: number;
+  name: string;
+  title: string;
+  description: string[];
+  size: string[];
+  color: string[];
+  availability: string;
+  price: number[];
+  category: string[];
+  stock: number[];
+  brand: string;
+  thumbnail: string;
+  images: string[];
+}
