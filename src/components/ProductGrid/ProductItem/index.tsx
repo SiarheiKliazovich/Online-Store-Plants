247b74156fae76eef810be299c9f405634e1755a
SiarheiKliazovich <alex849317@gmail.com>
2022-12-17 17:41:43 +0300

    fix: fixed mistakes with css.map

--- src/components/ProductGrid/ProductItem/index.tsx
+++ src/components/ProductGrid/ProductItem/index.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
-import { Product } from "../../../interface/interface";
+import { Product } from "../../../interface/product";
 import { Link } from "react-router-dom";
 import "./ProductItem.css";
 
