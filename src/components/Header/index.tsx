83b8405366950ad40effede3bcbf0c065a01f354
anast-ananko <anast.ananko@gmail.com>
2022-12-26 21:26:32 +0200

    fix: fix some paddings

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,8 @@
+import { FunctionComponent } from "react";
 import "./header.scss";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header: FunctionComponent = () => {
   return (
     <div className="header">
       <div className="container">
--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,7 +1,16 @@
+import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+
 import "./notFound.scss";
 
-const NotFound = () => {
-  return <div className="not-found"></div>;
+const NotFound: FunctionComponent = () => {
+  return (
+    <div className="not-found vh">
+      <Link className="not-found__button" to="/">
+        Main
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
